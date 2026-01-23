89b7621796bf5a159ccf1e5568a6d72217743456
muhsi29 <muhsin@blackitechs.com>
2026-01-23 16:45:09 +0530

    Product Page Done

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -18,8 +18,9 @@ export class Products {
   }
 
   industrialProducts: any[] = [];
+  irrigationProducts: any[] = [];
 
-  ngOnInit() {
+  ngOnInit() : void {
     this.industrialProducts = [
       {
         id: 1,
@@ -183,6 +184,42 @@ export class Products {
         ]
       }
     ];
+    this.irrigationProducts = [
+      {
+        id: 1,
+        title: 'Applications',
+        features: [
+           'Highly recommended for irrigation of closely spaced crops like Tomoto, Brinjal, Banana, Sugarcane, Cotton, Vegetables, Spices etc..',
+           'Excellent choice for greenhouses and nurseries.',
+           'Ideal for surface as well as sub surface installations.'
+        ]
+      },
+      {
+        id: 2,
+        title: 'Online Lateral',
+        description: 'Available in class-1 & class-2, made of high grade virgin polyethylene conforming to IS 12786, resistant to chemicals and fertilizer used in agriculture. It is used as lateral pipe in various micro irrigation systems such as Drip, Micro sprinkler, Micro sprayer, Jets, Foggers etc.',
+      },
+      {
+        id: 3,
+        title: 'Inline Drip Lateral',
+        description: 'Our Inline Laterals are manufactured in accordance with Indian Standard IS 13488. Our drippers are conveniently spaced to ensure smooth and even distribution of water. We use non-collapsible cylindrical dripper with unique clog resistant design. The available dripper spacings are 30cm, 40cm, 45cm, 50cm, 60cm, 90cm & 120cm. The flow rate will be 8 LPH',
+      },
+      {
+        id: 4,
+        title: 'Drip Irrigation Tape (Flat Drip Tubes)',
+        description: 'This is a thin wall drip/line used for drip irrigation Suitable for closely spaced short term crops and for above ground laying purposes. Normally the drip tape is flat and becomes round shape when water flows through it. Usually operated at lower pressure than other types of pressurised irrigation methods.',
+      }
+    ];
+  }
+    getProductImageUrl_1(productId: number): string {
+    const imageMap: { [key: number]: string } = {
+      1:'../../../assets/products/ir-1.jpg',
+      2: '../../../assets/products/ir-2.jpg',
+      3: '../../../assets/products/ir-3.jpg',
+      4: '../../../assets/products/ir-4.jpg'
+    };
+    
+    return imageMap[productId];
   }
 
   getProductImageUrl(productId: number): string {
@@ -215,5 +252,86 @@ export class Products {
   shouldReverse(index: number): boolean {
     return index % 2 === 0;
   }
+  pipeData = [
+  { od: '12', class1: '0.6 - 0.8', class2: '0.9 - 1.1' },
+  { od: '16', class1: '0.8 - 1.0', class2: '1.1 - 1.3' },
+  { od: '20', class1: '0.9 - 1.1', class2: '1.2 - 1.4' }
+];
+
+  fittings = [
+    { name: 'Starter', img: '../../../assets/products/irs-1.jpg' },
+    { name: 'Washer', img: '../../../assets/products/irs-2.jpg' },
+    { name: 'TEE', img: '../../../assets/products/irs-3.jpg' },
+    { name: 'TAP', img: '../../../assets/products/irs-4.jpg' },
+    { name: 'Dripper', img: '../../../assets/products/irs-5.jpg' },
+    { name: 'Elbow', img: '../../../assets/products/irs-6.jpg' },
+    { name: 'End cap', img: '../../../assets/products/irs-7.jpg' },
+    { name: 'Micro spinkler', img: '../../../assets/products/irs-8.jpg' },
+    { name: 'Adapter', img:'../../../assets/products/irs-9.jpg' },
+    { name: 'Air valves', img: '../../../assets/products/irs-10.jpg' },
+    { name: 'Connecter', img: '../../../assets/products/irs-11.jpg' },
+    { name: 'Filter screen disc', img: '../../../assets/products/irs-12.jpg' }
+  ];
+  products = [
+    {
+      title: 'PVC Fills',
+      image: '../../../assets/products/sp-1.jpg' 
+    },
+    {
+      title: 'Cooling Tower IP 55 Extended Shaft  Motor ',
+      image: '../../../assets/products/sp-2.jpg' 
+    },
+    {
+      title: 'Aluminium Sprinkler',
+      image: '../../../assets/products/sp-3.jpg' 
+    },
+    {
+      title: 'Cast Iron Butterfly Valves',
+      image: '../../../assets/products/sp-4.jpg' 
+    }
+  ];
+  other_products = [
+    {
+      title: 'CIY Strainers',
+      image: '../../../assets/products/op-1.jpg'
+    },
+    {
+      title: 'SS/Brass Ball Valves',
+      image: '../../../assets/products/op-2.jpg'
+    },
+    {
+      title: 'PP Valves & Products',
+      image: '../../../assets/products/op-3.jpg'
+    },
+    {
+      title: 'PP NOZZLES',
+      image: '../../../assets/products/op-4.jpg'
+    },
+    {
+      title: 'CI Non Return Valve',
+      image: '../../../assets/products/op-5.jpg'
+    },  {
+      title: 'Energy Efficient FRP Cooling Tower  Fan',
+      image: '../../../assets/products/op-6.jpg'
+    },
+
+    {
+      title: 'CI Foot Valve',
+      image: '../../../assets/products/op-7.jpg'
+    },
+       {
+      title: 'SS Hydraulic Fittings & Bellows',
+      image: '../../../assets/products/op-8.jpg'
+    },   {
+      title: 'PP Valves & Products',
+      image: '../../../assets/products/op-9.jpg'
+    },   {
+      title: 'Hydraulic Hoses and fittings',
+      image: '../../../assets/products/op-10.jpg'
+    },  {
+      title: 'SS Clamps',
+      image: '../../../assets/products/op-11.jpg'
+    }
+  ];
 
 }
